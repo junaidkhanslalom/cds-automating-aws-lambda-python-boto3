98010ba8a2cb0820fb9f94c80bba9436313d45da
Junaid Khan <junaid.khan@slalom.com>
2022-01-29 22:32:13 -0500

    dynamo db example added

---
+++ dynamodb/dynamodb-boto3.py
@@ -0,0 +1,7 @@
+# Create cloud9 Environment, then add code
+# Sample Scripts has been added in this folder :) 
+
+# Bookmark link for DynamoDB example
+
+# git clone https://github.com/linuxacademy/content-lambda-boto3.git
+
